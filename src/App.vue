b0af9e849ce31fc7c15ed712f1debf9d6ef034b1
abshir <abshir.ahmed@outlook.com>
2020-10-17 04:05:06 +0100

    A Chuck Norris joke every 10s

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <github-link />
+  <fake-chuck-norris-tweet />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FakeChuckNorrisTweet from './components/FakeChuckNorrisTweet';
+import GithubLink from './components/GithubLink';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    GithubLink,
+    FakeChuckNorrisTweet
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/GithubLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    class="absolute top-0 right-0"
+    href="https://github.com/abshirahmed/fake-chuck-norris-tweets"
+    target="_blank"
+  >
+    <i class="mdi mdi-github text-5xl"></i>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'GithubLink'
+};
+</script>
